8833dcb97578e1951c268d8dceeddeffbb7a80c3
Justin Di <justindi15@yahoo.com>
2018-03-26 16:03:39 -0700

    added username and password to employee and guest

--- hotel.sql
+++ hotel.sql
@@ -15,6 +15,8 @@ CREATE TABLE Location (
 
 CREATE TABLE Employee (
   employeeId INT(11) AUTO_INCREMENT,
+  username   CHAR(100)     NOT NULL UNIQUE,
+  password   CHAR(255)     NOT NULL,
   name       CHAR(100)     NOT NULL,
   phoneNum   CHAR(10)      NOT NULL,
   position   CHAR(100)     NOT NULL,
@@ -59,6 +61,8 @@ CREATE TABLE Booking (
 CREATE TABLE Guest (
   guestId       INT(11) AUTO_INCREMENT,
   name          CHAR(100) NOT NULL,
+  username   CHAR(100)     NOT NULL UNIQUE,
+  password   CHAR(255)     NOT NULL,
   phoneNum      CHAR(10)  NOT NULL,
   creditCardNum CHAR(16)  NOT NULL,
   PRIMARY KEY (guestId)
@@ -97,33 +101,33 @@ VALUES (5, '1579 Port St', '7783435475');
 # EMPLOYEE
 INSERT INTO
   Employee (
-    employeeId, name, phoneNum, position, hourlyRate, startDate, locationId)
+    employeeId, username, password, name, phoneNum, position, hourlyRate, startDate, locationId)
 VALUES (
-  1, 'Arthur Tse', '7781234561', 'Bellboy', 10.75, '2018-02-15', 1);
+  1, 'arthurtse', 'password', 'Arthur Tse', '7781234561', 'manager', 10.75, '2018-02-15', 1);
 
 INSERT INTO
   Employee (
-    employeeId, name, phoneNum, position, hourlyRate, startDate, locationId)
+    employeeId, username, password, name, phoneNum, position, hourlyRate, startDate, locationId)
 VALUES (
-  2, 'Laks V.S. Lakshmanan', '7751234721', 'Bellboy Manager', 15.25, '2018-02-16', 1);
+  2, 'laksvslakshmanan', 'password', 'Laks V.S. Lakshmanan', '7751234721', 'Bellboy Manager', 15.25, '2018-02-16', 1);
 
 INSERT INTO
   Employee (
-    employeeId, name, phoneNum, position, hourlyRate, startDate, locationId)
+    employeeId, username, password, name, phoneNum, position, hourlyRate, startDate, locationId)
 VALUES (
-  3, 'John Doe', '7751234521', 'Janitor', 10.50, '2018-02-16', 2);
+  3, 'johndoe', 'password', 'John Doe', '7751234521', 'Janitor', 10.50, '2018-02-16', 2);
 
 INSERT INTO
   Employee (
-    employeeId, name, phoneNum, position, hourlyRate, startDate, locationId)
+    employeeId, username, password, name, phoneNum, position, hourlyRate, startDate, locationId)
 VALUES (
-  4, 'Jack Doe', '7751284521', 'Janitor', 10.50, '2018-02-16', 2);
+  4, 'jackdoe', 'password', 'Jack Doe', '7751284521', 'Janitor', 10.50, '2018-02-16', 2);
 
 INSERT INTO
   Employee (
-    employeeId, name, phoneNum, position, hourlyRate, startDate, locationId)
+    employeeId, username, password, name, phoneNum, position, hourlyRate, startDate, locationId)
 VALUES (
-  5, 'Jill Doe', '7751294521', 'Janitor', 10.50, '2018-02-16', 3);
+  5, 'jilldoe', 'password', 'Jill Doe', '7751294521', 'Janitor', 10.50, '2018-02-16', 3);
 
 # FACILITY
 INSERT INTO
@@ -184,20 +188,20 @@ INSERT INTO Booking (bookingId, cost, inDate, outDate, numOfGuests)
 VALUES (5, 350, '2018-01-18', '2018-01-20', 3);
 
 # GUESTS
-INSERT INTO Guest (guestId, name, phoneNum, creditCardNum)
-VALUES (1, 'Oprah Winfrey', '7783435475', '4550123456789012');
+INSERT INTO Guest (guestId, name, username, password, phoneNum, creditCardNum)
+VALUES (1, 'Oprah Winfrey', 'oprahwinfrey', 'password', '7783435475', '4550123456789012');
 
-INSERT INTO Guest (guestId, name, phoneNum, creditCardNum)
-VALUES (2, 'Will Smith', '7834921933', '4550123456789012');
+INSERT INTO Guest (guestId, name, username, password, phoneNum, creditCardNum)
+VALUES (2, 'Will Smith', 'willsmith', 'password', '7834921933', '4550123456789012');
 
-INSERT INTO Guest (guestId, name, phoneNum, creditCardNum)
-VALUES (3, 'Barack Obama', '7781245365', '4550123456789012');
+INSERT INTO Guest (guestId, name, username, password, phoneNum, creditCardNum)
+VALUES (3, 'Barack Obama', 'barackobama', 'password', '7781245365', '4550123456789012');
 
-INSERT INTO Guest (guestId, name, phoneNum, creditCardNum)
-VALUES (4, 'Donald Trump', '8849392023', '4550123456789012');
+INSERT INTO Guest (guestId, name, username, password, phoneNum, creditCardNum)
+VALUES (4, 'Donald Trump', 'donaldtrump', 'password', '8849392023', '4550123456789012');
 
-INSERT INTO Guest (guestId, name, phoneNum, creditCardNum)
-VALUES (5, 'Arthur Tse', '9913324434', '8849392023034500');
+INSERT INTO Guest (guestId, name, username, password, phoneNum, creditCardNum)
+VALUES (5, 'Arthur Tse', 'arthurtse', 'password', '9913324434', '8849392023034500');
 
 # Reserve
 INSERT INTO Reserve (
