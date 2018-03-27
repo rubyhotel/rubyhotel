09cdfa25d81431dd14cab7d8c32942a5cf2108a5
scveloso <samtveloso@gmail.com>
2018-03-27 14:12:06 -0700

    Added link in home

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   resources :reserves
   resources :employees
   resources :guests
